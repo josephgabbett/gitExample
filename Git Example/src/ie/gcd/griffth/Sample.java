36f57560cd8aa2b15b8a60a68c69d7d0b0fd9289
joeyg <joeyg@172.19.1.122>
2013-04-10 13:11:25 +0100

    Intial commit

---
+++ Git Example/src/ie/gcd/griffth/Sample.java
@@ -0,0 +1,17 @@
+package ie.gcd.griffth;
+
+public class Sample {
+	
+	public Sample() {
+		
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
