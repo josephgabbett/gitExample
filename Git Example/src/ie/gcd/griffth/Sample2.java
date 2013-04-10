d38173ea8e5dd7a8ff32f81a48c947493bd56b8b
joeyg <joeyg@172.19.1.122>
2013-04-10 13:39:50 +0100

    updating master

---
+++ Git Example/src/ie/gcd/griffth/Sample2.java
@@ -0,0 +1,17 @@
+package ie.gcd.griffth;
+
+public class Sample2 {
+
+	public Sample2() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
